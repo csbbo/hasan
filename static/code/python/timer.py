2a5e234b00d7cc73a0f0b0700492bdd34cfd4d61
chen <chenshaobo272@gmail.com>
2020-02-20 11:39:57 +0800

    add timer demo

---
+++ static/code/python/timer.py
@@ -0,0 +1,31 @@
+import datetime
+import time
+import subprocess
+from apscheduler.schedulers.blocking import BlockingScheduler as Scheduler
+
+def work1():
+    now = datetime.datetime.now()
+    now_time = now.strftime('%Y-%m-%d %H:%M:%S')
+    data = subprocess.check_output('curl "localhost:3000/user?scheduler=now"', shell=True)
+    print(data.decode('utf-8'))
+
+def work2():
+    now = datetime.datetime.now()
+    now_time = now.strftime('%Y-%m-%d %H:%M:%S')
+    print(now_time)
+    time.sleep(2)
+
+def work3():
+    now = datetime.datetime.now()
+    now_time = now.strftime('%Y-%m-%d %H:%M:%S')
+    print("每天定时任务"+now_time)
+
+def job():
+    scheduler = Scheduler()
+    scheduler.add_job(work1, 'interval', seconds=2)
+    scheduler.add_job(work2, 'interval', seconds=3)
+    scheduler.add_job(work3, 'cron', day_of_week='0-6', hour=11, minute=9, second=0)
+    scheduler.start()
+
+if __name__ == '__main__':
+    job()
